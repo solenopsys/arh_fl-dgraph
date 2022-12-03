b36279e3dc925d66ba48a73c5af9c9c89aa32cc5
AlexStorm <info@alexstorm.ru>
2022-12-03 21:23:13 +0530

    Init commit

---
+++ src/lib/model.ts
@@ -0,0 +1,139 @@
+import {Observable} from 'rxjs';
+
+import {DataItemInterface, DataListInterface} from "./dataprovider";
+import {ProviderToken} from "@angular/core";
+
+export enum FacetType {
+  NUMBER,
+}
+
+export enum FieldType {
+  STRING,
+  STRING_MULTILINE,
+  BOOLEAN,
+  NUMBER,
+  UID,
+  FLOAT,
+  DATE,
+  PASSWORD,
+  EUID,
+  DATETIME,
+  FILE,
+  CODE,
+}
+
+export enum ViewType {
+  ELECTRONIC_SYMBOL,
+  ELECTRONIC_LAYOUT,
+  PART_SYMBOL,
+  CELL,
+}
+
+export const ICONS_TYPES: any = {};
+
+ICONS_TYPES[FieldType.DATE] =
+  '/assets/icons/01-Interface-Essential/21-Date-Calendar/calendar-date.svg';
+ICONS_TYPES[FieldType.EUID] =
+  '/assets/icons/01-Interface-Essential/27-Link-Unlink/hyperlink-3.svg';
+ICONS_TYPES[FieldType.STRING_MULTILINE] =
+  '/assets/icons/01-Interface-Essential/34-Text-Formating/text-style.svg';
+ICONS_TYPES[FieldType.STRING] =
+  '/assets/icons/01-Interface-Essential/34-Text-Formating/text-style.svg';
+ICONS_TYPES[FieldType.UID] =
+  '/assets/icons/01-Interface-Essential/28-Share/share.svg';
+ICONS_TYPES[FieldType.BOOLEAN] =
+  '/assets/icons/01-Interface-Essential/13-Controls/settings-toggle-horizontal.svg';
+ICONS_TYPES[FieldType.NUMBER] =
+  '/assets/icons/01-Interface-Essential/51-Paginate/paginate-filter-10.svg';
+ICONS_TYPES[FieldType.DATETIME] =
+  '/assets/icons/01-Interface-Essential/18-Time/watch-time.svg';
+ICONS_TYPES[FieldType.FILE] =
+  '/assets/icons/11-Content/02-Books/book-download-1.svg';
+ICONS_TYPES[FieldType.CODE] =
+  '/assets/icons/04-Programing-Apps-Websites/01-Programing/programming-browser-1.svg';
+
+export interface Facet {
+  name: string;
+  type: FacetType;
+}
+
+export interface EntityLink {
+  facets?: Facet[];
+  titleField: string;
+  multiple: boolean;
+}
+
+export interface FormField {
+  title: string;
+  type: FieldType;
+  link?: EntityLink;
+  format?: string;
+  key: string;
+}
+
+export interface Column {
+  name: string;
+  key: string;
+}
+
+export interface DataView {
+  title: string;
+  component: ViewType;
+  dataFrom: string;
+}
+
+export interface FormConfig {
+  title: string;
+  nameField?: string;
+  fields: FormField[];
+  commands: string[];
+  views?: DataView[];
+}
+
+export interface DataPageConfig extends FormConfig {
+  title: string;
+  listQ: string;
+  deleteQ?: string;
+  massCommand?:boolean
+  dataProvider: ProviderToken<DataListInterface | DataItemInterface>
+}
+
+export interface EntityTitle {
+  title: string;
+  uid: string;
+}
+
+export interface DataProvider {
+  initObserver(str: Observable<string>): Observable<EntityTitle[]>;
+
+  byId(id: string): Observable<string>;
+}
+
+
+export interface Article {
+  name: string;
+  version: string;
+}
+
+//todo move this to another module
+export enum TextNodeType {
+  PARAGRAPH = 'PARAGRAPH',
+  HEADER1 = 'HEADER1',
+  HEADER2 = 'HEADER2',
+  HEADER3 = 'HEADER3',
+  IMAGE = 'IMAGE',
+  CODE = 'CODE'
+}
+
+//todo move this to another module
+export interface ContentNode {
+  uid?: string;
+  before?: string;
+  type: TextNodeType;
+  value: string;
+}
+
+export interface ArticleVersion {
+  articleId: string;
+  blocks: ContentNode[];
+}
---
+++ src/lib/query-gen.spec.ts
@@ -0,0 +1,102 @@
+import {QueryGen} from './query-gen';
+import {ArticleVersion, DataPageConfig, FacetType, FieldType} from './model';
+import {TextNodeType} from "@hyperconverged/fui/editor";
+
+
+export const TEST_CONFIG: DataPageConfig = {
+    title: 'Time',
+    fields: [
+        {key: 'uid', title: 'UID', type: FieldType.UID},
+        {key: 'rom.time', title: 'Time', type: FieldType.NUMBER},
+        {key: 'description', title: 'Description', type: FieldType.STRING},
+        {
+            key: 'processLink',
+            title: 'Process',
+            type: FieldType.EUID,
+            link: {titleField: 'rom.process', multiple: false, facets: [{name: 'count', type: FacetType.NUMBER}]}
+        },
+        {key: 'date', title: 'Date', type: FieldType.DATE},
+    ],
+    commands: ['edit'],
+    listQ: 'has(rom.time)'
+};
+
+describe('QueryGen', () => {
+        it('should create an instance', () => {
+            expect(new QueryGen(TEST_CONFIG)).toBeTruthy();
+        });
+
+
+        it('find query correct', () => {
+            expect(QueryGen.find('bla', 'rom.time')).toEqual('{ results(func:  regexp(rom.time,/^.*bla.*$/i)) {rom.time uid }}');
+        });
+
+        it('any', () => {
+            expect(QueryGen.find('bl', 'rom.time')).toEqual('{ results(func:  has(rom.time)) {rom.time uid }}');
+        });
+
+
+        it('update correct', () => {
+            const qGen = new QueryGen(TEST_CONFIG);
+            const values = {
+                'rom.time': 10,
+                description: 'test description',
+                processLink: [{uid: '0xc38b', 'processLink|count': 10}],
+                date: '2021-03-24',
+            };
+            expect(qGen.update('0xc36b', values)).toEqual(
+                '{ set { <0xc36b> <rom.time> "10" . \n' +
+                '<0xc36b> <description> "test description" . \n' +
+                '<0xc36b> <processLink> <0xc38b> (count=10)  . \n' +
+                '<0xc36b> <date> "2021-03-24" . \n' +
+                ' }  }');
+        });
+
+        it('update correct empty', () => {
+            const qGen = new QueryGen(TEST_CONFIG);
+            const values = {
+                'rom.time': 10,
+                description: 'test description',
+                processLink: [{uid: ''}],
+                date: '2021-03-24',
+            };
+            expect(qGen.update('0xc36b', values)).toEqual(
+                '{ set { <0xc36b> <rom.time> "10" . \n' +
+                '<0xc36b> <description> "test description" . \n' +
+                '<0xc36b> <date> "2021-03-24" . \n' +
+                ' }  }');
+        });
+
+        it('new text version correct', () => {
+            const values: ArticleVersion = {
+                articleId: '32423',
+                blocks: [
+                    {type: TextNodeType.PARAGRAPH, value: 'first', uid: 'bla'},
+                    {type: TextNodeType.PARAGRAPH, value: 'second', before: 'before'}
+                ],
+            };
+            const s = '{ set { _:art <fragment> <32423> . \n' +
+                '_:textVersion <version_date>  "2021-09-03T13:22:38.296Z" .\n' +
+                '_:art <blocks> <bla> (ord=0) . \n' +
+                '_:block1 <type> "PARAGRAPH" . \n' +
+                '_:block1 <before> <before> . \n' +
+                '_:block1 <data> "second" . \n' +
+                '_:art <blocks> _:block1 (ord=1) . \n' +
+                ' }  }';
+            expect(QueryGen.newTextVersion(values)).toEqual(s);
+
+        });
+
+
+        it('escape correct', () => {
+
+            const s = '{"pins":[{"name":"pin1","x":20,"y":0}]}';
+            const escapeOk = QueryGen.escape(s);
+            console.log('ESCAPE OK', escapeOk);
+            expect(escapeOk).toEqual('{\\"pins\\":[{\\"name\\":\\"pin1\\",\\"x\\":20,\\"y\\":0}]}');
+
+        });
+
+
+    }
+);
