2e175903b9f614749c3e01f18e08ba3c18fea795
AlexStorm <info@alexstorm.ru>
2022-12-05 08:22:36 +0530

    pathes fixes

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/data/dgraph',
+  coverageDirectory: '../../../coverage/packages/libs/dgraph',
   transform: {
     '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
